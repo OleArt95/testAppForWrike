eea4e47b5601fa645e605de8ae99657acbd4cca1
oleg <Wickodin@gmail.com>
2019-09-08 18:08:38 +0200

    Add comments, fix assert method for Twitter button

--- src/test/java/WrikeSeleniumTest.java
+++ src/test/java/WrikeSeleniumTest.java
@@ -26,7 +26,7 @@ public class WrikeSeleniumTest {
         clickOnRandomAnswerInFirstQuestion(driver);
         clickOnRandomAnswerInSecondQuestion(driver);
         clickOnRandomAnswerInThirdQuestion(driver);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //Ожидание в 5 секунд для завершения отправки результатов
         clickOnSubmitResultsBtn(driver);
         checkTwitterBtnInFooter(driver);
     }
@@ -47,9 +47,11 @@ public class WrikeSeleniumTest {
         WebElement btnCreateMyWrikeAccount = driver.findElement(By.xpath("//*[@id=\"modal-pro\"]/form/label[2]/button"));
         btnCreateMyWrikeAccount.click();
 
+        //Метод явного ожидания видимости абзаца "Help us provide you the best possible experience:"
         WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form/h3")));
 
+        //Проверка того, что URL сменился с https://wrike.com
         Assert.assertNotEquals("https://www.wrike.com/", driver.getCurrentUrl());
     }
 
@@ -78,15 +80,17 @@ public class WrikeSeleniumTest {
         WebElement btnSubmitResults = driver.findElement(By.xpath("//div[2]/div/div[2]/div/form/button"));
         btnSubmitResults.click();
 
+        //Метод явного ожидания того, что кнопка "Submit Results" невидима. Он нужен для дальнейшего выполнения теста
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.invisibilityOf(btnSubmitResults));
 
+        //Проверка того, что кнопка "Submit Results" не отображается. Нужна для дальнейшего выполнения теста
         Assert.assertFalse(btnSubmitResults.isDisplayed());
     }
 
     private void checkTwitterBtnInFooter(WebDriver driver) {
         WebElement btnTwitter = driver.findElement(By.xpath("//div[3]/div/div[1]/div/ul/li[1]"));
-        btnTwitter.isDisplayed();
+        Assert.assertTrue(btnTwitter.isDisplayed()); //Проверка того, что кнопка Twitter в Follow Us отображается
 
 //        String twitterLink = driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div[1]/div/ul/li[1]/a")).getText();
 //        Assert.assertTrue(twitterLink.contains("https://twitter.com/wrike"));
